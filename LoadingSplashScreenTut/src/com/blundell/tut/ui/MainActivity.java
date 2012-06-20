4b4c9fa3c08b05b8af29cb3e51eb63dd9c7969fa
=Paul Blundell Mac <blundell.app.dev@gmail.com>
2012-06-20 20:09:57 +0100

    init

---
+++ LoadingSplashScreenTut/src/com/blundell/tut/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.blundell.tut.ui;
+
+import com.blundell.tut.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
